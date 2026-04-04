de8646dd8aba2254cafb5bcca6f8a0675ad1d014
pch <cksgma3218@gmail.com>
2026-04-04 16:27:32 +0900

    feat: initialize Android project with Gradle 9.3.1, Compose, and Hilt dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
     id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -43,10 +44,6 @@ android {
 
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     packaging {
